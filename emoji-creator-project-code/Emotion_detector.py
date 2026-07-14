1104f2bacbc498f6791d2534f201f82b5031cdac
Anas4571 <anas.siddique4571@gmail.com>
2026-07-14 20:31:37 +0530

    Initial Commit

---
+++ emoji-creator-project-code/Emotion_detector.py
@@ -0,0 +1,7 @@
+import numpy as np
+import cv2
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout, Flatten, Conv2D, MaxPooling2D
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
